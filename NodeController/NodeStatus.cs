96cc34af36d958cbb93b1edb0d7630df15db34a4
JeffM2501 <JeffM2501@gmail.com>
2016-08-22 10:06:59 -0700

    add node status message

---
+++ NodeController/NodeStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NodeController
+{
+	public class NodeStatus
+	{
+		public Process ProcHandle = null;
+
+		public string GlobalID = string.Empty;
+		public string PublicName = string.Empty;
+
+		public Dictionary<string, string> Properties = new Dictionary<string, string>();
+
+		//protected List<string>
+	}
+}
